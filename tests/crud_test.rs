661ff8081043d235465d93cfa071cf0b53daeac4
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-11-06 18:48:54 +0900

    move sample code to test

---
+++ tests/crud_test.rs
@@ -0,0 +1,29 @@
+use self::models::*;
+use diesel::{insert_into, prelude::*};
+use new_tax_account_backend::*;
+
+#[test]
+fn test_crud() {
+    use self::schema::posts::dsl::*;
+
+    let connection = &mut establish_connection();
+
+    insert_into(posts)
+        .values((title.eq("test title"), body.eq("tes"), published.eq(true)))
+        .execute(connection)
+        .unwrap();
+
+    let results = posts
+        .filter(published.eq(true))
+        .select(Post::as_select())
+        .load(connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!(
+            "id:{:?}, title:{}, body: {}, published: {}",
+            post.id, post.title, post.body, post.published
+        );
+    }
+}

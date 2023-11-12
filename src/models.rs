eedaa6e985a5f83486ee96f06c212fe958e57912
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-11-12 15:24:19 +0900

    schema: update ddl

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
 
 #[derive(Queryable, Selectable)]
@@ -7,5 +8,10 @@ pub struct Post {
     pub id: Option<i32>,
     pub title: String,
     pub body: String,
+    pub category_id: Option<i32>,
+    pub author: Option<String>,
     pub published: bool,
+    pub good_count: i32,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,10 +1,25 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    category (id) {
+        id -> Nullable<Integer>,
+        name -> Text,
+        description -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
     posts (id) {
         id -> Nullable<Integer>,
         title -> Text,
         body -> Text,
+        category_id -> Nullable<Integer>,
+        author -> Nullable<Text>,
         published -> Bool,
+        good_count -> Integer,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(category, posts,);

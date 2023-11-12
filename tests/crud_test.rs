527d17122cf3c9cf7495cacd6c9694df1a4d6897
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-11-12 14:44:51 +0900

    update test

--- tests/crud_test.rs
+++ tests/crud_test.rs
@@ -42,20 +42,21 @@ fn test_insert() {
 }
 
 #[test]
-fn test_select_simple() {
-    use self::schema::posts::dsl::*;
+fn test_select_all_columns() {
+    use self::schema::posts::dsl as posts;
 
     let mut connection = get_connection();
     let _ = insert_post(&mut connection, "title1", "body1", true);
-
-    // Confirm that there is no data present.
-    let results = posts
+    let results = posts::posts
         .select(Post::as_select())
         .load(&mut connection)
         .expect("Error loading posts");
+
     assert!(results.len() == 1);
+
     let head = results.get(0);
     assert!(head.is_some());
+
     let head = head.unwrap();
     assert!(head.title == "title1");
     assert!(head.body == "body1");
@@ -63,21 +64,69 @@ fn test_select_simple() {
 }
 
 #[test]
-fn test_select_filter_by_name() {
-    use self::schema::posts::dsl::*;
+fn test_select_specified_columns() {
+    use self::schema::posts::dsl as posts;
+
+    let mut connection = get_connection();
+    let _ = insert_post(&mut connection, "title1", "body1", true);
+    let results = posts::posts
+        .select((posts::title, posts::body))
+        .load::<(String, String)>(&mut connection)
+        .expect("Error loading posts");
+
+    assert!(results.len() == 1);
+
+    let head = results.get(0).unwrap();
+    assert!(head.0 == "title1");
+    assert!(head.1 == "body1");
+}
+
+#[test]
+fn test_select_into_struct() {
+    use self::schema::posts::dsl as posts;
+
+    let mut connection = get_connection();
+    let _ = insert_post(&mut connection, "title1", "body1", true);
+
+    #[derive(Queryable, Debug)]
+    struct PostTitleBody {
+        id: Option<i32>,
+        title: String,
+        body: String,
+    }
+
+    let results = posts::posts
+        .select((posts::id, posts::title, posts::body))
+        .load::<PostTitleBody>(&mut connection)
+        .expect("Error loading posts");
+
+    assert!(results.len() == 1);
+
+    let head = results.get(0).unwrap();
+    assert!(head.id.is_some());
+    assert!(head.title == "title1");
+    assert!(head.body == "body1");
+}
+
+#[test]
+fn test_select_by_filter() {
+    use self::schema::posts::dsl as posts;
 
     let mut connection = get_connection();
     let _ = insert_post(&mut connection, "title1", "body1", true);
     let _ = insert_post(&mut connection, "title2", "body2", false);
 
-    let results = posts
-        .filter(title.eq("title2"))
+    let results = posts::posts
+        .filter(posts::title.eq("title2"))
         .select(Post::as_select())
         .load(&mut connection)
         .expect("Error loading posts");
+
     assert!(results.len() == 1);
+
     let head = results.get(0);
     assert!(head.is_some());
+
     let head = head.unwrap();
     assert!(head.title == "title2");
     assert!(head.body == "body2");
@@ -85,121 +134,19 @@ fn test_select_filter_by_name() {
 }
 
 #[test]
-fn test_select_filter_by_names() {
-    use self::schema::posts::dsl::*;
+fn test_select_by_multiple_filters() {
+    use self::schema::posts::dsl as posts;
 
     let mut connection = get_connection();
     let _ = insert_post(&mut connection, "title1", "body1", true);
-    let _ = insert_post(&mut connection, "title2", "body2", true);
+    let _ = insert_post(&mut connection, "title2", "body2", false);
 
-    let results = posts
-        .filter(title.eq("title2").and(published.eq(true)))
+    let results = posts::posts
+        .filter(posts::title.eq("title2"))
+        .filter(posts::body.eq("body1"))
         .select(Post::as_select())
         .load(&mut connection)
         .expect("Error loading posts");
-    assert!(results.len() == 1);
-    let head = results.get(0);
-    assert!(head.is_some());
-    let head = head.unwrap();
-    assert!(head.title == "title2");
-    assert!(head.body == "body2");
-    assert!(head.published);
+
+    assert!(results.len() == 0);
 }
-
-// #[test]
-// fn test_basic_crud() {
-//     use self::schema::posts::dsl::*;
-
-//     init();
-
-//     let mut connection = get_connection();
-
-//     // Confirm that there is no data present.
-//     let results = posts
-//         .select(Post::as_select())
-//         .load(&mut connection)
-//         .expect("Error loading posts");
-//     assert!(results.is_empty());
-
-//     // Insert two rows.
-//     insert_into(posts)
-//         .values((
-//             title.eq("test title1"),
-//             body.eq("test body1"),
-//             published.eq(true),
-//         ))
-//         .execute(&mut connection)
-//         .unwrap();
-
-//     insert_into(posts)
-//         .values((
-//             title.eq("test title2"),
-//             body.eq("test body2"),
-//             published.eq(false),
-//         ))
-//         .execute(&mut connection)
-//         .unwrap();
-
-//     // Confirm that there are two rows present.
-//     let results = posts
-//         .select(Post::as_select())
-//         .load(&mut connection)
-//         .expect("Error loading posts");
-//     assert!(results.len() == 2);
-
-//     // Search by specifying search criteria.
-//     let results = posts
-//         .filter(published.eq(false))
-//         .select(Post::as_select())
-//         .load(&mut connection)
-//         .expect("Error loading posts");
-//     assert!(results.len() == 1);
-
-//     // Test that the retrieved record values are correct.
-//     let head = results.get(0);
-//     assert!(head.is_some());
-//     let head = head.unwrap();
-//     assert!(head.title == "test title2");
-//     assert!(head.body == "test body2");
-//     assert!(head.published == false);
-
-//     // Select only the specified columns by tuple.
-//     let results = posts
-//         .filter(published.eq(true))
-//         .select((title, body))
-//         .load::<(String, String)>(&mut connection)
-//         .expect("Error loading posts");
-//     assert!(results.len() == 1);
-//     let head = results.get(0).unwrap();
-//     assert!(head.0 == "test title1");
-//     assert!(head.1 == "test body1");
-
-//     // Select only the specified columns by struct.
-//     #[derive(Queryable, Debug)]
-//     struct PostTitleBody {
-//         id: Option<i32>,
-//         title: String,
-//         body: String,
-//     }
-//     let results = posts
-//         .filter(published.eq(true))
-//         .select((id, title, body))
-//         .load::<PostTitleBody>(&mut connection)
-//         .expect("Error loading posts");
-//     assert!(results.len() == 1);
-//     let head = results.get(0).unwrap();
-//     assert!(head.title == "test title1");
-//     assert!(head.body == "test body1");
-
-//     let update_result = diesel::update(posts.filter(id.eq(head.id)))
-//         .set((title.eq("new title"), body.eq("new body")))
-//         .execute(&mut connection);
-
-//     assert!(update_result.is_ok());
-//     assert!(update_result.unwrap() == 1);
-
-//     struct UpdatePost {
-//         title: String,
-//         body: String,
-//     }
-// }
